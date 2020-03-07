89e231dea1554b60cf1be251dce3242d806a72c6
quadratus <amiteshwarsidhu7@gmail.com>
2020-03-08 00:38:05 +0530

    Misc updates

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import { Route } from "react-router-dom";
+import Navbar from "./Navbar";
+import Login from "./Login";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const Button = ({ className, onClick, children }) => (
+  <button className={className} onClick={onClick}>
+    {children}
+  </button>
+);
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchTerm: "What"
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        <Navbar />
+        <Route path="/" exact component={Login} />
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/Login.js
@@ -0,0 +1,10 @@
+import React from "react";
+import Batchflow from "./Batchflow";
+
+const Login = () => (
+  <div>
+    <Batchflow region="flow" />
+  </div>
+);
+
+export default Login;
